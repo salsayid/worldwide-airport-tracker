ac6e535d063c68e5835f030980a38bee8ac7a82d
efriedman4 <efriedman4@huskers.unl.edu>
2024-05-01 11:17:45 -0500

    added ids to the kivy elements in the credentials screen

--- travel_planner_app/travel_main.py
+++ travel_planner_app/travel_main.py
@@ -2,11 +2,18 @@ from kivy.app import App
 from kivy.core.window import Window
 from kivy.modules import inspector
 from kivy.uix.screenmanager import Screen
+from sqlalchemy.exc import SQLAlchemyError
 
 from database import FinalDatabase
 
 class StartUpScreen(Screen):
-    pass
+    def submit_credentials(self):
+        try:
+            url = FinalDatabase.construct_mysql_url(self.ids.authority.text, int(self.ids.port_number.text), self.ids.database_name.text, self.ids.database_username.text, self.ids.database_password.text)
+            self.operator_database = FinalDatabase(url)
+            self.session = self.operator_database.create_session()
+        except SQLAlchemyError as exception:
+            print(exception)
 
 class LoadingScreen(Screen):
     pass
@@ -24,12 +31,10 @@ class PrepareItineraryScreen(Screen):
     pass
 
 class TravelPlannerApp(App):
+
     def build(self):
         inspector.create_inspector(Window, self)  # For inspection (press control-e to toggle).
 
 if __name__ == '__main__':
-    url = FinalDatabase.construct_mysql_url('localhost', 3306, 'name', 'username', 'password')
-    milestone_1_database = FinalDatabase(url)
-    session = milestone_1_database.create_session()
     app = TravelPlannerApp()
     app.run()
