5a2d9db03b10f77a7699cfe43028bdff4c00d521
salsayid2 <salsayid2@huskers.unl.edu>
2024-05-03 23:53:45 -0500

    unit test for airport app

--- second_tracking_app/credentials.py
+++ second_tracking_app/credentials.py
@@ -1,12 +1,7 @@
 PORT = '3306'
-DATABASE_NAME = 'milestone1'
-USERNAME = 'root'
-PASSWORD = '!7og89TreeF9'
+DATABASE_NAME = 'salsayid2'
+USERNAME = 'salsayid2'
+PASSWORD = 'cho1ooWaew9u'
 API_KEY = '726d68785d644545b20b909688d093e6'
 
 
-
-'''
-nmcvay2
-OFahgh0eeGi6
-'''
--- second_tracking_app/test_airport_main.py
+++ second_tracking_app/test_airport_main.py
@@ -1,7 +1,21 @@
 import unittest
-from unittest.mock import MagicMock, patch, Mock
-from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy import create_engine, MetaData, Table
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from unittest.mock import patch, MagicMock
+from io import StringIO
+from datetime import datetime
+from second_tracking_app.airport_installer import AirportApp
+
+class TestAirportApp(unittest.TestCase):
+    @patch('builtins.input', side_effect=['1', 'Airport Name', 'ABC',
+                                          'Location', '2', 'City Name', 'Entity', 'Location', '3', 'ABC', '2024-05-03'])
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_main_function(self, mock_stdout, mock_input):
+        app = AirportApp()
+        app.build()
+
+        output = mock_stdout.getvalue()
+        self.assertIn("Airport added successfully!", output)
+        self.assertIn("City added successfully!", output)
+        self.assertIn("Forecast for ABC on 2024-05-03:", output)
+
+if __name__ == '__main__':
+    unittest.main()
